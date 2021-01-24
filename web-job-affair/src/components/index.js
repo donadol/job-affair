b1dabe938ec3cadc7fb91da1159f73432848b881
Laura Donado <ldonado11@gmail.com>
2021-01-23 20:52:21 -0500

    Download template and delete unnecessary files

---
+++ web-job-affair/src/components/index.js
@@ -0,0 +1,20 @@
+// Cards
+import ChartCard from "./Cards/ChartCard.vue";
+import NavTabsCard from "./Cards/NavTabsCard.vue";
+import StatsCard from "./Cards/StatsCard.vue";
+import JobCard from "./Cards/JobCard.vue";
+
+// Tables
+import NavTabsTable from "./Tables/NavTabsTable.vue";
+import OrderedTable from "./Tables/OrderedTable.vue";
+import SimpleTable from "./Tables/SimpleTable.vue";
+
+export {
+  ChartCard,
+  NavTabsCard,
+  StatsCard,
+  JobCard,
+  NavTabsTable,
+  OrderedTable,
+  SimpleTable
+};
---
+++ web-job-affair/src/views/Dashboard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="content">
+    <div class="md-layout">
+      <div
+        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
+        v-for="job in this.jobs.results"
+        :key="job.id"
+      >
+        <job-card
+          :type="job.type"
+          :title="job.objective"
+          :date="job.deadline"
+          :image="
+            job.organizations.lenght > 0 ? null : job.organizations[0].picture
+          "
+        >
+        </job-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { JobCard } from "@/components";
+import { mapActions, mapState } from "vuex";
+
+export default {
+  components: {
+    JobCard
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState("jobs", ["jobs"])
+  },
+  mounted() {
+    this.getJobs({ size: 9, offset: 0 });
+  },
+  methods: {
+    ...mapActions("jobs", ["getJobs"])
+  },
+  computed: {
+    ...mapState("jobs", ["jobs"])
+  }
+};
+</script>
