71de281ef2bc2529344eae04cc7eccea0c6f27f2
Laura Donado <ldonado11@gmail.com>
2021-01-26 22:03:33 -0500

    Delete tables component

--- web-job-affair/src/components/index.js
+++ web-job-affair/src/components/index.js
@@ -4,11 +4,6 @@ import NavTabsCard from "./Cards/NavTabsCard.vue";
 import StatsCard from "./Cards/StatsCard.vue";
 import JobCard from "./Cards/JobCard.vue";
 
-// Tables
-import NavTabsTable from "./Tables/NavTabsTable.vue";
-import OrderedTable from "./Tables/OrderedTable.vue";
-import SimpleTable from "./Tables/SimpleTable.vue";
-
 // User
 import UserCard from "./UserProfile/UserCard.vue";
 
@@ -24,9 +19,6 @@ export {
   NavTabsCard,
   StatsCard,
   JobCard,
-  NavTabsTable,
-  OrderedTable,
-  SimpleTable,
   UserCard,
   CompanyProfile,
   JobProfile,
