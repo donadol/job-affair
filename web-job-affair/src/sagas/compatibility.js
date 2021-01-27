796205011e8f7d1f36a48ae442d90ec196a5f28a
Laura Donado <ldonado11@gmail.com>
2021-01-26 21:43:31 -0500

    Add saga for compatibility

---
+++ web-job-affair/src/sagas/compatibility.js
@@ -0,0 +1,33 @@
+import { takeLatest, call, put } from "redux-saga/effects";
+import { Request } from "@/services/services";
+import { COMPATIBILITY_API } from "@/config/constants";
+
+export function* handleGetCompatibility({ payload }) {
+  try {
+    const res = yield call(Request.get, `compatibility`, {
+      baseURL: COMPATIBILITY_API,
+      params: {
+          jobid: payload.jobid,
+          username: payload.username
+      }
+    });
+
+    var compatibility = res.data;
+
+    if (compatibility.message && (compatibility.message === "not found" || compatibility.message === "API not working.")) {
+      compatibility = {
+        match: 0,
+        skills: []
+      };
+    }
+
+    yield put({ type: "compatibility/update", payload: compatibility });
+  } catch (e) {
+    console.log("get compatibility error: ", e.toString(), e.stack);
+    yield put({ type: "compatibility/error", payload: e });
+  }
+}
+
+export default function* compatibility() {
+  yield takeLatest("compatibility/getCompatibility", handleGetCompatibility);
+}
--- web-job-affair/src/sagas/index.js
+++ web-job-affair/src/sagas/index.js
@@ -2,5 +2,6 @@ import jobs from "./jobs";
 import people from "./people";
 import job from "./job";
 import user from "./user";
+import compatibility from "./compatibility";
 
-export default [jobs, people, job, user];
+export default [jobs, people, job, user, compatibility];
