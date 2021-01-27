9d0fe7b5791fddf3de272fc3d6d263a076d74dd1
Laura Donado <ldonado11@gmail.com>
2021-01-26 21:16:55 -0500

    Add better responses to function and handle errors

--- server-job-affair/application/compatibility/compatibility.py
+++ server-job-affair/application/compatibility/compatibility.py
@@ -9,10 +9,17 @@ def job_user_info(job_id, username):
     job_resp = requests.get('https://torre.co/api/opportunities/' + job_id)
     user_resp = requests.get('https://torre.bio/api/bios/' + username)
 
-    if job_resp.status_code == 400 or user_resp.status_code == 400:
-        return make_response('API not working.', 400)
+    if job_resp.status_code == 404 or user_resp.status_code == 404:
+        return make_response(
+            jsonify(
+                    {"message": "not found"}
+                ), 404)
 
-    print(user_resp)
+    if job_resp.status_code != 200 or user_resp.status_code != 200:
+        return make_response(
+            jsonify(
+                    {"message": "API not working"}
+                ), 400)
 
     user_strs = user_resp.json()['strengths']
     user_ints = user_resp.json()['interests']
@@ -25,6 +32,9 @@ def job_user_info(job_id, username):
 @compatibility_bp.route('/compatibility/', methods=['GET'])
 def compatibility_test():
     user_job_info = job_user_info(request.args.get('jobid'), request.args.get('username'))
+    if 'user_strs' not in user_job_info:
+        return user_job_info
+
     match = []
     found = False
     count = 0
@@ -48,6 +58,4 @@ def compatibility_test():
         found = False
         total = total + 1
 
-    obj_ret = {'match': float(count / total) * 100, 'skills': match}
-
-    return make_response(jsonify(obj_ret), 200)
+    return make_response(jsonify({'match': float(count / total) * 100, 'skills': match}), 200)
