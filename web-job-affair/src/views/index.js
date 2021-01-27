1563af81ad2dd2a0fdf337a27807bc3d421ab99b
Laura Donado <ldonado11@gmail.com>
2021-01-26 19:53:09 -0500

    Delete unnecessary components

--- web-job-affair/src/views/index.js
+++
@@ -1,7 +0,0 @@
-// Cards
-import UserCard from "../views/UserProfile/UserCard.vue";
-
-// Forms
-import EditProfileForm from "../views/UserProfile/EditProfileForm.vue";
-
-export { UserCard, EditProfileForm };
