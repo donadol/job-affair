f59152a6845c7e6372d91dbe8047ed6eabeaf102
Laura Donado <ldonado11@gmail.com>
2021-01-27 17:16:46 -0500

    Add dist and static files

--- server-job-affair/application/__init__.py
+++ server-job-affair/application/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 def init_app():
     """Initialize the core application"""
 
-    app = Flask(__name__, instance_relative_config=False)
+    app = Flask(__name__, static_folder='../web-job-affair/dist/', instance_relative_config=False)
     app.config.from_object('config.Config')
 
     with app.app_context():
