e7dafd29c5eb320f20de78c09bc389170b97ebb1
Laura Donado <ldonado11@gmail.com>
2021-01-26 23:11:41 -0500

    Add compatibility api

--- web-job-affair/src/config/constants.js
+++ web-job-affair/src/config/constants.js
@@ -1,3 +1,4 @@
 export const SEARCH_API = "https://search.torre.co/";
 export const TORRE_JOBS_API = "https://torre.co/api/";
 export const TORRE_USER_API = "https://torre.bio/api/";
+export const COMPATIBILITY_API = "http://127.0.0.1:5000/";
