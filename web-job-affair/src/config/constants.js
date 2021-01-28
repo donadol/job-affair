a6f30a3fa61c8b76860ac4aed3a13623cedb6b57
Laura Donado <ldonado11@gmail.com>
2021-01-27 19:18:41 -0500

    Fix api url

--- web-job-affair/src/config/constants.js
+++ web-job-affair/src/config/constants.js
@@ -1,4 +1,4 @@
 export const SEARCH_API = "https://search.torre.co/";
 export const TORRE_JOBS_API = "https://torre.co/api/";
 export const TORRE_USER_API = "https://torre.bio/api/";
-export const COMPATIBILITY_API = "https://job-affair.herokuapp.com/";
+export const COMPATIBILITY_API = "https://api-job-affair.herokuapp.com/";
